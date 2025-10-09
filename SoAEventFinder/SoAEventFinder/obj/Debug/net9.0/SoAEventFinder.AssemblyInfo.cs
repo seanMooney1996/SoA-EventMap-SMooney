0ec6b0230a729cfc2eed91a82ab9c8528e4d0e7b
Sean Mooney <seanmooney1996@gmail.com>
2025-10-09 20:20:46 +0100

    Add .gitignore to main

--- SoAEventFinder/SoAEventFinder/obj/Debug/net9.0/SoAEventFinder.AssemblyInfo.cs
+++ SoAEventFinder/SoAEventFinder/obj/Debug/net9.0/SoAEventFinder.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoAEventFinder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46e4cdfc005997775b245513520b42ce5b709055")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31364b241ed8003426ae17db9ec0fcf387458b1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoAEventFinder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoAEventFinder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
