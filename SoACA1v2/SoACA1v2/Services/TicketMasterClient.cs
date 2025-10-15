88490ca2f32e8f2b0e583d814f450f0f48d987a7
Sean Mooney <seanmooney1996@gmail.com>
2025-10-15 20:37:46 +0100

    Bring google changes from its branch

---
+++ SoACA1v2/SoACA1v2/Services/TicketMasterClient.cs
@@ -0,0 +1,22 @@
+﻿using SoACA1v2.DataModels;
+using System.Net.Http.Json;
+
+namespace SoACA1v2.Services
+{
+    public class TicketMasterClient : HttpClientBase
+    {
+        public TicketMasterClient(HttpClient http, IConfiguration config)
+            : base(http, config, "TicketMasterAPI") { }
+
+        public async Task<RootObject?> GetEventsByCity(string city)
+        {
+            var url = $"events.json?apikey={_apiKey}&city={city}";
+            return await GetAsync<RootObject>(url);
+        }
+        public async Task<RootObject?> GetEventsByGenreAndCountry(string countryCode, string genreId)
+        {
+            var url = $"events.json?apikey={_apiKey}&countryCode={countryCode}&genreId={genreId}";
+            return await GetAsync<RootObject>(url);
+        }
+    }
+}
